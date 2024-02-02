0a72357c6a852c826d4aa3714179b38c4c4f162b
Clarissa Adela Garcia <124195524+Adelacg25@users.noreply.github.com>
2024-02-02 11:19:24 -0600

    begining

---
+++ nbody.jl
@@ -0,0 +1,22 @@
+module nbody 
+
+
+using Printf
+
+# Constants
+const solar_mass = 4 * pi * pi
+const days_per_year = 365.24
+
+# construct body 
+
+# account for momentum 
+
+# create the sun 
+
+# kicks 
+
+# account for energy 
+
+# planets - jupiter - saturn - uranis - neptune 
+
+# dont forget to end module 
